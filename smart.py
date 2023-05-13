e912b798de8b0d17e51c4f419190971f250230c6
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2023-05-13 19:08:18 +0200

    openai basic text integration

---
+++ smart.py
@@ -0,0 +1,29 @@
+import openai
+from os import environ
+
+
+def smart(question, bank_info):
+    openai.api_key = environ.get('OPENAI_API_KEY')
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
+        {"role": "system", "content":
+        """
+        You are an AI that can answer questions about bank transactions.
+        You are connected to a bank account and can answer questions about the current balance and common transactions.
+        You must answer succinctly and in plain English the questions asked.
+        """},
+        {"role": "system", "content":
+        f"""
+        Here is the current information about the bank account:
+        {bank_info}
+        """},
+        {"role": "user", "content": question}
+    ])
+    print(completion.choices[0].message.content)
+    return 'I am too dumb to answer that question.'
+
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    smart('What is the current balance?', 'Current balance: $1000')
+    pass
