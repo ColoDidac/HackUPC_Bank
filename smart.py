1db32fcd9f991959dcdf2f405d8b663a16de1433
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2023-05-13 19:13:01 +0200

    style

--- smart.py
+++ smart.py
@@ -1,5 +1,5 @@
-import openai
 from os import environ
+import openai
 
 
 def smart(question, bank_info):
@@ -8,7 +8,8 @@ def smart(question, bank_info):
         {"role": "system", "content":
         """
         You are an AI that can answer questions about bank transactions.
-        You are connected to a bank account and can answer questions about the current balance and common transactions.
+        You are connected to a bank account """ \
+        """and can answer questions about the current balance and common transactions.
         You must answer succinctly and in plain English the questions asked.
         """},
         {"role": "system", "content":
@@ -26,4 +27,3 @@ if __name__ == '__main__':
 
     load_dotenv()
     smart('What is the current balance?', 'Current balance: $1000')
-    pass
